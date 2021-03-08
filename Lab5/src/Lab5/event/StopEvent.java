26e6f913047f835bba1239ce5162b231f491f86e
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:43:49 +0100

    Update StopEvent.java

--- Lab5/src/Lab5/event/StopEvent.java
+++ Lab5/src/Lab5/event/StopEvent.java
@@ -1,5 +1,14 @@
 package Lab5.event;
 
+/**
+ * Stops the simulation
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class StopEvent {
 
 }
