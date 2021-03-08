dc8efe3335bb67af1900e20fb3cc7043902bc8af
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:42:54 +0100

    Update PickEvent.java

--- Lab5/src/Lab5/event/PickEvent.java
+++ Lab5/src/Lab5/event/PickEvent.java
@@ -1,5 +1,14 @@
 package Lab5.event;
 
+/**
+ * Description
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class PickEvent {
 
 }
