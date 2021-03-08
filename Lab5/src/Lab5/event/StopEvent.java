a7c037ac1134d29d529536658f2b6f13820fdd71
bergdaahl <79858996+bergdaahl@users.noreply.github.com>
2021-03-08 20:46:53 +0100

    Update StopEvent.java

--- Lab5/src/Lab5/event/StopEvent.java
+++ Lab5/src/Lab5/event/StopEvent.java
@@ -1,14 +1,20 @@
 package Lab5.event;
 
+import Lab5.simulation.Event;
+import Lab5.simulation.State;
+
 /**
  * Stops the simulation
- * 
+ *
  * @author Alex Bergdahl, 
  * @author Kim Eriksson, 
  * @author Peggy Khialie, 
  * @author Rasmus Jacobsen
  *
  */
-public class StopEvent {
+public class StopEvent extends Event {
 
+    StopEvent(State state) {
+        state.stop = true;
+    }
 }
