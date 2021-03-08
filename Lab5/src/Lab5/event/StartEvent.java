70902c8101c98d1c564c2e2d7e7a1d24035490fd
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:37:26 +0100

    Update StartEvent.java

--- Lab5/src/Lab5/event/StartEvent.java
+++ Lab5/src/Lab5/event/StartEvent.java
@@ -1,14 +1,24 @@
+
 package Lab5.event;
 
 import Lab5.simulation.View;
-import Lab5.state.*;
+import Lab5.state.SuperMarket;
 
+/**
+ * Starts the SuperMarket
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class StartEvent {
     SuperMarket superMarket;
     View view;
 
-    StartEvent() {
-        superMarket = new SuperMarket();
+    StartEvent(int seed, int cashLim, int CLim) {
+        superMarket = new SuperMarket(seed, cashLim, CLim);
 
         // EventQue variable not added to superMarket yet
         superMarket.EventQueue.addEvent(new ArrivalEvent(0));
