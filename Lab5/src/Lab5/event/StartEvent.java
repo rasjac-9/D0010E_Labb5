dc2758fd6c786f263dc5ab7f2ce2e1a567aa56b6
bergdaahl <79858996+bergdaahl@users.noreply.github.com>
2021-03-08 18:43:06 +0100

    Update StartEvent.java

--- Lab5/src/Lab5/event/StartEvent.java
+++ Lab5/src/Lab5/event/StartEvent.java
@@ -1,5 +1,19 @@
 package Lab5.event;
 
-public class StartEvent {
+import Lab5.simulation.View;
+import Lab5.state.*;
 
+public class StartEvent {
+    SuperMarket superMarket;
+    View view;
+
+    StartEvent() {
+        superMarket = new SuperMarket();
+
+        // EventQue variable not added to superMarket yet
+        superMarket.EventQueue.addEvent(new ArrivalEvent(0));
+       
+        // view in StartEvent (?)
+        view = new View();
+    }
 }
