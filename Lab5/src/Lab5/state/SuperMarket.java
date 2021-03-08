d40c552af8c85c2081f5c59bae761f7af68e03a9
bergdaahl <79858996+bergdaahl@users.noreply.github.com>
2021-03-08 22:56:24 +0100

    Update SuperMarket.java

--- Lab5/src/Lab5/state/SuperMarket.java
+++ Lab5/src/Lab5/state/SuperMarket.java
@@ -1,6 +1,6 @@
 /**
  * Description
- * 
+ *
  * @author Alex Bergdahl, 
  * @author Kim Eriksson, 
  * @author Peggy Khialie, 
@@ -14,103 +14,105 @@ import Lab5.simulation.State;
 
 public class SuperMarket extends State {
 
-	boolean openForBis;
-	int occupiedCashReg, lostCustomer, inStore, lambda, seed;
-	double kmax, kmin, pmin, pmax;
+    boolean openForBis;
+    int occupiedCashReg, lostCustomer, inStore, lambda, seed, shopped;
+    double kmax, kmin, pmin, pmax;
 
-	final int cashRegLimit;
-	final int customerLimit;
-	
-	public TimeState ts;
-	public FIFO cashQueue;
-	
-	/**
-	 * Starts the SuperMarket
-	 * 
-	 * @param regLimit - the limit on how many cashRegisters that exists
-	 * @param CLimit   - the limit on how many customers aloud in store
-	 */
-	public SuperMarket(int seed, int regLimit, int CLimit, int lambda, int pmin, int pmax, int kmin, int kmax) {
-		this.kmax = kmax;
-		this.kmin = kmin;
-		this.pmin = pmin;
-		this.pmax = pmax;
-		this.lambda = lambda;
-		this.seed = seed;
-		cashRegLimit = regLimit;
-		customerLimit = CLimit;
+    final int cashRegLimit;
+    final int customerLimit;
 
-		openForBis = true;
-		occupiedCashReg = 0;
-		lostCustomer = 0;
-		inStore = 0;
+    public TimeState ts;
+    public FIFO cashQueue;
 
-		ts = new TimeState(this);
-	}
+    /**
+     * Starts the SuperMarket
+     *
+     * @param regLimit - the limit on how many cashRegisters that exists
+     * @param CLimit   - the limit on how many customers aloud in store
+     */
+    public SuperMarket(int seed, int regLimit, int CLimit, int lambda, int pmin, int pmax, int kmin, int kmax) {
+        this.kmax = kmax;
+        this.kmin = kmin;
+        this.pmin = pmin;
+        this.pmax = pmax;
+        this.lambda = lambda;
+        this.seed = seed;
+        cashRegLimit = regLimit;
+        customerLimit = CLimit;
 
-	/**
-	 * Adds a customer to the store
-	 * 
-	 * @throws ArithmeticException - if customerLimtit is reached
-	 */
-	public void addCustomer() throws ArithmeticException {
-		if (inStore == customerLimit) {
-			throw new ArithmeticException();
-		} else {
-			inStore += 1;
-		}
-	}
+        openForBis = true;
+        occupiedCashReg = 0;
+        lostCustomer = 0;
+        inStore = 0;
 
-	/**
-	 * Removes customer from store
-	 */
-	public void customerLeft() {
-		if (inStore != 0) {
-			inStore -= 1;
-		}
-	}
+        ts = new TimeState(this);
+    }
 
-	/**
-	 * Adds to amount of customers that wasn't aloud in the store
-	 */
-	public void lostCustomer() {
-		lostCustomer += 1;
-	}
+    /**
+     * Adds a customer to the store
+     *
+     * @throws ArithmeticException - if customerLimtit is reached
+     */
+    public void addCustomer() throws ArithmeticException {
+        if (inStore == customerLimit) {
+            throw new ArithmeticException();
+        } else {
+            inStore += 1;
+        }
+    }
 
-	/**
-	 * Adds to occupiedCashReg
-	 */
-	public void addToCashReg() throws ArithmeticException {
-		if (occupiedCashReg == cashRegLimit) {
-			throw new ArithmeticException();
-		} else {
-			occupiedCashReg += 1;
-		}
-	}
+    /**
+     * Removes customer from store
+     */
+    public void customerLeft() {
+        if (inStore != 0) {
+            inStore -= 1;
+            shopped += 1;
+            occupiedCashReg -= 1;
+        }
+    }
 
-	/**
-	 * Decrease occupiedCashReg
-	 * 
-	 * @throws ArithmeticException - if cashRegLimit is reached
-	 */
-	public void freeCashReg() {
-		if (occupiedCashReg != 0) {
-			occupiedCashReg -= 1;
-		}
-	}
+    /**
+     * Adds to amount of customers that wasn't aloud in the store
+     */
+    public void lostCustomer() {
+        lostCustomer += 1;
+    }
 
-	/**
-	 * @return the amount of occupiedCashReg
-	 */
-	public int getAvailableCashReg() {
-		return cashRegLimit - occupiedCashReg;
-	}
+    /**
+     * Adds to occupiedCashReg
+     */
+    public void addToCashReg() throws ArithmeticException {
+        if (occupiedCashReg == cashRegLimit) {
+            throw new ArithmeticException();
+        } else {
+            occupiedCashReg += 1;
+        }
+    }
 
-	public void closeStore() {
-		openForBis = false;
-	}
+    /**
+     * Decrease occupiedCashReg
+     *
+     * @throws ArithmeticException - if cashRegLimit is reached
+     */
+    public void freeCashReg() {
+        if (occupiedCashReg != 0) {
+            occupiedCashReg -= 1;
+        }
+    }
 
-	public boolean getOpenForBis() {
-		return openForBis;
-	}
+    /**
+     * @return the amount of occupiedCashReg
+     */
+    public int getAvailableCashReg() {
+        return cashRegLimit - occupiedCashReg;
+    }
+
+    public void closeStore() {
+        openForBis = false;
+    }
+
+    public boolean getOpenForBis() {
+        return openForBis;
+    }
 }
