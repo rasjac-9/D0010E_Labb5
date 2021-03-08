f8fd64280ff7fdb97f601e4f11d5a84b4508f4c9
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:59:18 +0100

    Update PickEvent.java

--- Lab5/src/Lab5/event/PickEvent.java
+++ Lab5/src/Lab5/event/PickEvent.java
@@ -1,14 +1,23 @@
+
 package Lab5.event;
+
+import Lab5.simulation.Event;
+import Lab5.state.SuperMarket;
 
 /**
  * Description
  * 
- * @author Alex Bergdahl, 
- * @author Kim Eriksson, 
- * @author Peggy Khialie, 
+ * @author Alex Bergdahl,
+ * @author Kim Eriksson,
+ * @author Peggy Khialie,
  * @author Rasmus Jacobsen
  *
  */
-public class PickEvent {
-
+public class PickEvent extends Event{
+	
+	public PickEvent(int ID, SuperMarket sm) {
+		customerID = ID;
+		s = sm;
+		time = s.ts.getPickTime();
+	}
 }
