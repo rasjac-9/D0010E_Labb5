38cfe800c0bbfcbba4f4f3e5f7598d757180a9af
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 19:05:07 +0100

    Update SuperMarket.java

--- Lab5/src/Lab5/state/SuperMarket.java
+++ Lab5/src/Lab5/state/SuperMarket.java
@@ -1,5 +1,52 @@
 package Lab5.state;
 
 public class SuperMarket {
-
+	
+	boolean openForBis;
+	int cashReg, lostCustomer, inStore;
+	
+	final int cashRegLimit;
+	final int customerLimit;
+	
+	public SuperMarket(int regLimit, int CLimit) {
+		cashRegLimit = regLimit;
+		customerLimit = CLimit;
+		
+		cashReg = regLimit;
+		lostCustomer = 0;
+		inStore = 0;
+		
+		TimeState time = new TimeState();
+		
+		// TODO: parhaps create a view ??
+	}
+	
+	public void addCustomer() {
+		inStore += 1;
+	}
+	
+	public void customerLeft() {
+		inStore -= 1;
+	}
+	
+	public void lostCustomer() {
+		lostCustomer += 1;
+	}
+	
+	public int addToCashReg() throws ArithmeticException{
+		if (cashReg == 0) {
+			throw new ArithmeticException();
+		}else {
+			cashReg -= 1;
+		}
+		
+		return cashReg;
+	}
+	
+	public void availableCashReg(){
+		if (cashReg != cashRegLimit) {
+			cashReg -= 1;
+		}
+	}
+	
 }
