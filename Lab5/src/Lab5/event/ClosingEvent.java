627a3968030cbf4fb8400e677a94dd5438dbb132
peggykhialie <79858997+peggykhialie@users.noreply.github.com>
2021-03-08 19:57:15 +0100

    Update ClosingEvent.java

--- Lab5/src/Lab5/event/ClosingEvent.java
+++ Lab5/src/Lab5/event/ClosingEvent.java
@@ -1,5 +1,41 @@
 package Lab5.event;
 
-public class ClosingEvent {
+import State.Customer;
+import State.State
 
+/**
+ * Tells when the supermarket closes and does not let customers in the store.
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
+
+public class ClosingEvent extends Event {
+  
+  private double time;
+  
+  public ClosingEvent(State state, EventQueue eventList, double time) {
+    super(state, eventQueue);
+    this.time = time;    
+  }
+
+  public void doMe() {
+    state.update(this);
+    state.getSuperMarket().setIsOpen(false);
+   }
+
+   public double getTime() {
+     return time;
+   }
+  
+   public String getName() {
+     return "Close";
+   }
+
+   public Customer getCustomer() {
+     return null;
+   }
 }
