fa1f49ed8c7d52b8a0c5b339a168b89106768ed4
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:55:08 +0100

    Update Event.java

--- Lab5/src/Lab5/simulation/Event.java
+++ Lab5/src/Lab5/simulation/Event.java
@@ -1,8 +1,20 @@
+
 package Lab5.simulation;
 
+/**
+ * A abstract Event
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class Event {
+	
 	public int time;
 	public int customerID;
+	public State s;
 	
 	public Event(){
 	}
