7add2a76fe2500f3d22970c73917d693de3c5dcc
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:29:48 +0100

    Update State.java

--- Lab5/src/Lab5/simulation/State.java
+++ Lab5/src/Lab5/simulation/State.java
@@ -2,6 +2,15 @@ package Lab5.simulation;
 
 import Lab5.event.EventQueue;
 
+/**
+ * Creates a abstract state
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class State {
 	boolean stop = false;
 	public EventQueue eventQueue = new EventQueue();
