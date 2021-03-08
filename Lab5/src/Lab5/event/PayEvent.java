c15c641845e50a2e1fb84896e697074e49bcc737
rasjac-9 <79523809+rasjac-9@users.noreply.github.com>
2021-03-08 20:42:35 +0100

    Update PayEvent.java

--- Lab5/src/Lab5/event/PayEvent.java
+++ Lab5/src/Lab5/event/PayEvent.java
@@ -1,5 +1,15 @@
+
 package Lab5.event;
 
+/**
+ * Description
+ * 
+ * @author Alex Bergdahl, 
+ * @author Kim Eriksson, 
+ * @author Peggy Khialie, 
+ * @author Rasmus Jacobsen
+ *
+ */
 public class PayEvent {
 
 }
