931495c7534bda201676b9e7853092dc44462fad
rasjac-9 <ragge@MSI.local>
2021-03-09 15:34:52 +0100

    change ArrivalEvent and ClosingEcent

--- Lab5/src/Lab5/event/ArrivalEvent.java
+++ Lab5/src/Lab5/event/ArrivalEvent.java
@@ -15,51 +15,34 @@ import Lab5.state.SuperMarket;
  * @author Rasmus Jacobsen
  *
  */
-
 public class ArrivalEvent extends Event {
 
-	Pick pickEvent;
-	double time;
-	Customer customer;
-
-
-
-	public Arrival(State state, EventQueue eventList, double time) {
-	   super(state, eventList);
-	   this.time = time;
-   
-	} public ArrivalEvent(int ID, State s) {
+	/**
+	 * Creates a arrival event
+	 * 
+	 * @param ID - customer id
+	 * @param s  - the supermarket
+	 */
+	public ArrivalEvent(int ID, State s) {
 		customerID = ID;
 		time = ((SuperMarket) s).ts.getArrivalTime();
 
 		effect();
 	}
 
-	public void doMe() {
-
-		state.update(this);
-
-		this.state.getSuperMarket().addCustomer(customer);
-
-		if (customer.getState() == CustomerState.inSuperMarket) {
-			double pickTime = this.time + state.getPickTime().next();
-			pickEvent = new Pick(this.state, this.eventList, customer, pickTime);
-			eventList.addEvent(pickEvent);
-		}
-		
-	} @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	public void effect() {
 		try {
 			((SuperMarket) s).addCustomer();
-			
+
 			PickEvent pick = new PickEvent(customerID, (SuperMarket) s);
 			ArrivalEvent arrival = new ArrivalEvent(customerID + 1, (SuperMarket) s);
-			
-		} catch(ArithmeticException e) {
+
+		} catch (ArithmeticException e) {
 			((SuperMarket) s).lostCustomer();
 			ArrivalEvent arrival = new ArrivalEvent(customerID, (SuperMarket) s);
 		}
-		
+
 	}
 
 	public double getTime() {
@@ -70,10 +53,7 @@ public class ArrivalEvent extends Event {
 		return "Arrival";
 	}
 
-	@Override
-	public Customer getCustomer() {
-		return customer;
-	} public int getCustomer() {
+	public int getCustomer() {
 		return customerID;
 	}
 }
--- Lab5/src/Lab5/event/ClosingEvent.java
+++ Lab5/src/Lab5/event/ClosingEvent.java
@@ -1,8 +1,10 @@
+
 package Lab5.event;
 
 import Lab5.simulation.State;
 import Lab5.simulation.Event;
 import Lab5.state.SuperMarket;
+
 /**
  * Tells when the supermarket closes and does not let customers in the store.
  * 
@@ -14,32 +16,22 @@ import Lab5.state.SuperMarket;
  */
 
 public class ClosingEvent extends Event {
-	
+
 	private double time;
-	
+
 	// if it shall be added to EventQueue when we run the program
-	public ClosingEvent(SuperMarket sm){
+	public ClosingEvent(SuperMarket sm) {
 		s = sm;
 		time = sm.ts.getCloseTime();
 		effect();
 	}
-	
-	public ClosingEvent(State state, EventQueue eventList, double time) {
-		//super(state, eventQueue);
-		this.time = time;
-	}
-	
+
 	public void effect() {
-		//TODO: add to eventQueue
-		
-		((SuperMarket)s).closeStore();
+		// TODO: add to eventQueue
+
+		((SuperMarket) s).closeStore();
 	}
-	
-	public void doMe() {
-		state.update(this);
-		state.getSuperMarket().closeStore();
-	}
-	
+
 	public double getTime() {
 		return time;
 	}
