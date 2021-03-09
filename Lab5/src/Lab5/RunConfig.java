e72aaa65f46cf0514b60c945d50a3406c478f746
rasjac-9 <ragge@MSI.local>
2021-03-09 15:21:02 +0100

    Add runConfig

---
+++ Lab5/src/Lab5/RunConfig.java
@@ -0,0 +1,13 @@
+package Lab5;
+
+public class RunConfig {
+	public int seed = 1234; 
+	
+	public int regLimit = 2; 
+	public int CLimit = 5; 
+	
+	public double lambda = 1.0; 
+	
+	public double pMin = 0.5, pMax = 1.0;
+	public double kMin = 2.0, kMax = 3.0;
+}
