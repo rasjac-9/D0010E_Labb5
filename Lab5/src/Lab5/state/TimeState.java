4282b628f565d9efcf096601eef5a43917a5bcd3
rasjac-9 <ragge@MSI.local>
2021-03-09 19:08:20 +0100

    cosmetic change to TS

--- Lab5/src/Lab5/state/TimeState.java
+++ Lab5/src/Lab5/state/TimeState.java
@@ -7,26 +7,26 @@ import Lab5.event.*;
 /**
  * Gives time to event and keep track of time
  * 
- * @author Alex Bergdahl, 
- * @author Kim Eriksson, 
- * @author Peggy Khialie, 
+ * @author Alex Bergdahl,
+ * @author Kim Eriksson,
+ * @author Peggy Khialie,
  * @author Rasmus Jacobsen
  *
  */
 public class TimeState {
-	
+
 	private UniformRandomStream ranStreamShop;
 	private ExponentialRandomStream expStream;
 	private UniformRandomStream ranStreamPick;
-	
+
 	protected double shopTime;
-	
+
 	public double regTime;
 	public double queueTime;
 	public double currentTime;
-	
+
 	private double closingTime;
-	
+
 	SuperMarket sm;
 
 	public TimeState(SuperMarket s) {
@@ -34,11 +34,11 @@ public class TimeState {
 		ranStreamShop = new UniformRandomStream(s.kmin, s.kmax, s.seed);
 		ranStreamPick = new UniformRandomStream(s.pmin, s.pmax, s.seed);
 		expStream = new ExponentialRandomStream(s.lambda, s.seed);
-		
+
 		regTime = 0.0;
-		queueTime = 0.0;		
+		queueTime = 0.0;
 		currentTime = 0.0;
-		
+
 		callClosingEvent();
 	}
 
@@ -59,29 +59,29 @@ public class TimeState {
 	}
 
 	public void togelRegTime() {
-		if(sm.occupiedCashReg > 0) {
+		if (sm.occupiedCashReg > 0) {
 			sm.emptyReg = false;
 		} else {
 			sm.emptyReg = true;
 		}
 	}
-	
+
 	public void togelQueueTime() {
-		if(!sm.cashQueue.isEmpty()) {
+		if (!sm.cashQueue.isEmpty()) {
 			sm.inQueue = true;
 		} else {
 			sm.inQueue = false;
 		}
 	}
-	
+
 	public void updateTime() {
 		currentTime += 0.01;
-		
-		if(sm.emptyReg) {
+
+		if (sm.emptyReg) {
 			regTime += 0.01;
 		}
-		
-		if(sm.inQueue) {
+
+		if (sm.inQueue) {
 			queueTime += 0.01;
 		}
 	}
