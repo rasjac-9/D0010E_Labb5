fb0cd9f6629d893b4b84380115fc878c784bd98a
bergdaahl <bergdahl0101@gmail.com>
2021-03-11 00:45:07 +0100

    SuperMarketConsole decimal fix

--- Lab5/src/Lab5/view/SuperMarketConsole.java
+++ Lab5/src/Lab5/view/SuperMarketConsole.java
@@ -4,6 +4,9 @@ import Lab5.simulation.Event;
 import Lab5.simulation.View;
 import Lab5.state.SuperMarket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Observable;
 
 public class SuperMarketConsole extends View {
@@ -15,20 +18,31 @@ public class SuperMarketConsole extends View {
 
 	@Override
 	public void update(Observable o, Object arg) {
+		DecimalFormat df2 = new DecimalFormat("#.##");
+		df2.setRoundingMode(RoundingMode.UP);
 
 		Event event = (Event) arg;
 		SuperMarket superMarket = (SuperMarket) event.s;
 
-		double currentTime = Math.round(event.time * 100.0) / 100.0;
-		double regTime = Math.round(superMarket.regTime * 100.0) / 100.0;
-		double queueTime = Math.round(superMarket.queueTime * 100.0) / 100.0;
+		BigDecimal currentTime = new BigDecimal(event.time).setScale(2, RoundingMode.HALF_UP);
+		BigDecimal regTime = new BigDecimal(superMarket.regTime).setScale(2, RoundingMode.HALF_UP);
+		BigDecimal queueTime = new BigDecimal(superMarket.queueTime).setScale(2, RoundingMode.HALF_UP);
 
 		if (event.getName() == "Start") {
 			System.out.println(event.time + "\t" + event.getName());
 		}
 		else if (event.getName() == "Stop") {
 			System.out.println(event.time + "\t" + event.getName());
-		} else {
+		}
+		else if (event.getName() == "Stänger") {
+			System.out.println(currentTime + "\t" + event.getName() + "\t\t---\t"
+					+ "Ö\t" + superMarket.getAvailableCashReg() + "\t" + regTime + "\t"
+					+ superMarket.getCustomers() + "\t" + superMarket.getShopped() + "\t"
+					+ superMarket.getLostCustomer() + "\t" + superMarket.cashQueue.queueCounter + "\t"
+					+ queueTime + "\t" + "\t" + superMarket.cashQueue.getSize() + "\t"
+					+ superMarket.cashQueue.getQue());
+		}
+		else {
 
 			// TODO: add time that there have been empty cashReg (EMPTYCASHREGTIME) 
 			// TODO: counter for how many customers had to FIFOque (FIFOQUECOUNTER)
