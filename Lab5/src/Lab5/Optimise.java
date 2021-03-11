9ddfdb4b6c13f1d400555ca7efe29b18656fd94c
rasjac-9 <ragge@MSI.local>
2021-03-11 14:50:07 +0100

    update

--- Lab5/src/Lab5/Optimise.java
+++ Lab5/src/Lab5/Optimise.java
@@ -1,6 +1,8 @@
 package Lab5;
 
 public class Optimise {
+	static String[] str2 = {"13", "2", "7", "3.0", "8.0", "0.6", "0.9", "0.35", "0.6", "false"};
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
