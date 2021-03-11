812c2e5596b991ac5c2242ecc7648581fe6ee57f
rasjac-9 <ragge@MSI.local>
2021-03-11 17:26:46 +0100

    comments to View

--- Lab5/src/Lab5/view/SuperMarketFile.java
+++ Lab5/src/Lab5/view/SuperMarketFile.java
@@ -13,31 +13,38 @@ import java.math.RoundingMode;
 import java.util.Observable;
 
 public class SuperMarketFile extends View {
-    @Override
-    public void update(Observable o, Object arg) {
-        Event event = (Event) arg;
-        SuperMarket superMarket = (SuperMarket) event.s;
 
-        BigDecimal currentTime = new BigDecimal(event.time).setScale(2, RoundingMode.HALF_UP);
-        BigDecimal regTime = new BigDecimal(superMarket.regTime).setScale(2, RoundingMode.HALF_UP);
-        BigDecimal queueTime = new BigDecimal(superMarket.queueTime).setScale(2, RoundingMode.HALF_UP);
+	/**
+	 * When a update happens prints to a file
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		Event event = (Event) arg;
+		SuperMarket superMarket = (SuperMarket) event.s;
 
-        if(!(event.getName() == "Stop")) {
-            try {
-                File file = new File("optimization.txt");
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+		BigDecimal currentTime = new BigDecimal(event.time).setScale(2, RoundingMode.HALF_UP);
+		BigDecimal regTime = new BigDecimal(superMarket.regTime).setScale(2, RoundingMode.HALF_UP);
+		BigDecimal queueTime = new BigDecimal(superMarket.queueTime).setScale(2, RoundingMode.HALF_UP);
 
-            try {
-                PrintWriter writer = new PrintWriter("optimization.txt");
-                writer.write(superMarket.getLostCustomer()+"\n"+currentTime+"\n"+regTime+"\n"+
-                        superMarket.cashQueue.queueCounter+"\n"+queueTime);
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		// Saves every event that happens except for the stop event
+		if (!(event.getName() == "Stop")) {
+			// Try to open a new file
+			try {
+				File file = new File("optimization.txt");
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// Try to print to the file
+			try {
+				PrintWriter writer = new PrintWriter("optimization.txt");
+				writer.write(superMarket.getLostCustomer() + "\n" + currentTime + "\n" + regTime + "\n"
+						+ superMarket.cashQueue.queueCounter + "\n" + queueTime);
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
